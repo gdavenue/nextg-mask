17efe64cbe54f5274aa2155144a9ba76ee4a15a9
gdavenue <gdavenue30@gmail.com>
2025-08-29 18:57:31 +0300

    sticky script fixed

--- src/js/sticky-header.js
+++ src/js/sticky-header.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header');
 
-  window.addEventListener('scroll', () => {
+  const checkScroll = () => {
     header.classList.toggle('header--sticky', window.scrollY > 10);
-  });
+  };
+
+  checkScroll();
+
+  window.addEventListener('scroll', checkScroll);
 });
